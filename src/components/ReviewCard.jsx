c333d84f84df39ac07b134bae73f0c7ebb6196da
Rafael Niebles <rafaelniebles2003@gmail.com>
2025-04-27 18:21:15 -0400

    updates

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,11 +1,86 @@
 import { FaHeart, FaRegHeart, FaEdit } from "react-icons/fa";
 import { Link } from "react-router-dom"
 import { Rating } from "../components/Rating";
+import { supabase, getSession } from "../scripts/client"
+import { useState, useEffect } from "react"
 
-export const ReviewCard = ({ reviewId, trackId, username, rating, content, liked, editable }) => {
-  const likeButton = liked
-    ? <span className="mt-5 text-sm flex items-center font-bold"><FaHeart className="mr-2 text-lg text-red-500" /> You liked this review</span>
-    : <span className="mt-5 text-sm flex items-center"><FaRegHeart className="mr-2 text-lg" /> Like this review</span>;
+export const ReviewCard = ({ reviewId, trackId, username, rating, content, editable }) => {
+  const [liked, setLiked] = useState(false)
+  const [likeReloads, setLikeReloads] = useState(0)
+
+  useEffect(() => {
+    async function loadLikeState() {
+      // check if logged in user has liked this review
+      const session = await getSession()
+      if (!session) {
+        return
+      }
+
+      // get liker id (our own)
+      const { user: { id: likerId } } = session
+
+      // check if we've liked this review
+      const { data } = await supabase
+        .from('likes')
+        .select('id') // only select id (lighter)
+        .eq('review_id', reviewId)
+        .eq('liker_id', likerId)
+        .maybeSingle(); // Expect 0 or 1 result 
+
+      setLiked(data ? true : false)
+    }
+
+    loadLikeState()
+  }, [reviewId, likeReloads])
+
+  async function handleLike() {
+    const session = await getSession()
+    if (!session) {
+      return
+    }
+
+    // get liker id (our own)
+    const { user: { id: likerId } } = session
+
+    // check if we've liked this review
+    const { data: likeCheck } = await supabase
+      .from('likes')
+      .select('id') // only select id (lighter)
+      .eq('review_id', reviewId)
+      .eq('liker_id', likerId)
+      .maybeSingle(); // Expect 0 or 1 result 
+
+    // if check returs something, delete like 
+    if (likeCheck) {
+      await supabase
+        .from('likes')
+        .delete()
+        .match({
+          review_id: reviewId,
+          liker_id: likerId
+        })
+        .select()
+    } else {
+      // otherwise create a like
+      await supabase
+        .from('likes')
+        .insert({
+          review_id: reviewId, // the review being liked
+          liker_id: likerId,   // the user doing the like
+        })
+    }
+
+    // mark update
+    setLikeReloads(likeReloads + 1)
+  }
+
+  const likeButton =
+    <button
+      onClick={handleLike}
+      className="mt-5 text-sm flex items-center">
+      {liked ? <FaHeart className="mr-2 text-lg text-red-500" /> : <FaRegHeart className="mr-2 text-lg" />}
+      <span>{liked ? "You liked this review" : "Like review"}</span>
+    </button>;
 
   const editButton = editable &&
     <Link
