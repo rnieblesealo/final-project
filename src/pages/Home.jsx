08c1a44fd5053bdd52ed9d1a450684eddaa95869
Rafael Niebles <rafaelniebles2003@gmail.com>
2025-04-27 19:14:23 -0400

    fetch new tracks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,56 @@
 import { SongCard } from "../components/SongCard";
-import sampleSong from "../data/sample-song.json"
+import { supabase } from "../scripts/client"
+import { getSpotifyTrack } from "../scripts/music-search"
 
 import { useState, useEffect } from "react"
 
 export const Home = () => {
+  const [songCards, setSongCards] = useState(null)
+
+  useEffect(() => {
+    async function loadSongsWithReviews() {
+      const { data } = await supabase
+        .from('reviews')
+        .select('review_track_id', { distinct: true }) // DISTINCT in Supabase
+
+      if (!data) {
+        return
+      }
+
+      // deduplicate
+      const uniqueTrackIds = [...new Set(data.map(row => row.review_track_id))];
+
+      const songCards = await Promise.all(
+        uniqueTrackIds?.map(async (trackId) => {
+          const {
+            name: trackName,
+            artists: [{ name: trackArtistName }],
+            album: { images: [, { url: trackImage }] }
+          } = await getSpotifyTrack(trackId)
+
+          return (
+            <SongCard
+              key={trackId}
+              name={trackName}
+              artist={trackArtistName}
+              rating={0}
+              ratingCount={0}
+              coverSrc={trackImage}
+            />
+          )
+        }))
+
+      setSongCards(songCards)
+    }
+
+    loadSongsWithReviews()
+
+  }, [])
+
+
   return (
     <div className="px-10">
-      <SongCard
-        name={sampleSong.name}
-        artist={sampleSong.artist}
-        rating={sampleSong.rating}
-        ratingCount={sampleSong.ratingCount}
-        coverSrc={sampleSong.coverSrc}
-      />
+      {songCards}
     </div>
   )
 }
